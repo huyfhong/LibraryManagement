82e94031b8f7d03d54b4c3d3247cacce30c98f90
huyfhong <phonghuy2004@gmail.com>
2026-02-04 09:20:02 +0700

    :sv2: them lop lib

---
+++ LibararyManagement.java
@@ -0,0 +1,18 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibararyManagement {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Da them sach thanh cong!");
+    }
+
+    public void displayBooks() {
+        for (Book b : books) {
+            System.out.println(b);
+        }
+    }
+}
